7eb2c1e1e4ee31403f9bb2bf63ed18b397cc56ed
DESKTOP-E6S0LE0\SKT012 <16interchange@gmail.com>
2024-08-12 16:11:41 +0900

    cctv 화면 연결 및 캡쳐기능 구현

--- lib/streaming_screen.dart
+++ lib/streaming_screen.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:front/theme/colors.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_screen_recording/flutter_screen_recording.dart';
 
 class StreamingScreen extends StatefulWidget {
   final String? camName;
@@ -17,114 +16,197 @@ class StreamingScreen extends StatefulWidget {
 }
 
 class _StreamingScreenState extends State<StreamingScreen> {
-
   ScreenshotController _screenshotController = ScreenshotController();
+  bool isRecording = false;
+  String? _outputFilePath;
+  late WebViewController _webViewController;
 
   @override
   void initState() {
     super.initState();
-    requestPermissions();
+    _requestPermissions();
   }
 
-  Future<void> requestPermissions() async {
-    if (await Permission.photos.request().isGranted &&
-        await Permission.manageExternalStorage.request().isGranted) {
-      // 권한이 허용됨
-    } else {
-      // 권한이 거부됨
-      print('11111111111111111');
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('저장소 권한이 필요합니다.')));
+  Future<void> _requestPermissions() async {
+    var videoStatus = await Permission.videos.status;
+
+    if (!videoStatus.isGranted) {
+      var statuses = await [Permission.videos].request();
+
+      if (statuses[Permission.videos]!.isDenied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('비디오 권한이 필요합니다. 설정에서 권한을 부여해주세요.'),
+            action: SnackBarAction(
+              label: '설정으로 가기',
+              onPressed: () {
+                openAppSettings();
+              },
+            ),
+          ),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-          backgroundColor: yelloMyStyle2,
-          appBar: AppBar(
-            backgroundColor: whiteMyStyle1,
-            title: Text(
-              '${widget.camName} 카메라 실시간',
-              style: TextStyle(
-                fontSize: 18.0,
-              ),
+      child: Scaffold(
+        backgroundColor: Colors.yellow[200],
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            '${widget.camName} 카메라 실시간',
+            style: TextStyle(
+              fontSize: 18.0,
             ),
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-
-              children: [
-                Screenshot(
-                  controller: _screenshotController,
-                  child: Container(
-                    height: 300,
-                    child: WebView(
-                      // initialUrl: 'http://honeybeecombo.ddns.net:8081',
-                      // initialUrl: '192.168.137.124:8081',
-                      initialUrl: 'https://pub.dev/packages/webview_flutter',
-                      javascriptMode: JavascriptMode.unrestricted,
-                    ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Screenshot(
+                controller: _screenshotController,
+                child: Container(
+                  height: 300,
+                  child: WebView(
+                    initialUrl: 'http://172.23.248.239:8081',
+                    javascriptMode: JavascriptMode.unrestricted,
+                    onWebViewCreated: (WebViewController webViewController) {
+                      _webViewController = webViewController;
+                    },
                   ),
                 ),
-
-                SizedBox(height: 16.0,),
-
-                Container(
-                  height: 250,
-                  child: GridView.count(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10,
-                    children: [
-                      GridButton(icon: Icons.video_camera_back_outlined, label: '녹화', onTap: () {}),
-                      GridButton(
-                        icon: Icons.camera_enhance_outlined,
-                        label: '캡쳐',
-                        onTap: () {
-                          Permission.storage.request();
-                          captureScreenshot();
-                          },
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-        )
+              ),
+              SizedBox(height: 16.0),
+              Container(
+                height: 250,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  children: [
+                    GridButton(
+                      icon: isRecording
+                          ? Icons.stop_circle_outlined
+                          : Icons.video_camera_back_outlined,
+                      label: isRecording ? '녹화 중지' : '녹화 시작',
+                      onTap: () {
+                        isRecording ? _stopRecording() : _startRecording();
+                      },
+                    ),
+                    GridButton(
+                      icon: Icons.camera_enhance_outlined,
+                      label: '캡쳐',
+                      onTap: _captureScreenshot,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
-  void captureScreenshot() async {
-    final status = await Permission.storage.status;
-    if (status.isGranted) {
-      final image = await _screenshotController.capture();
-      if (image != null) {
-        final directory = await getExternalStorageDirectory();
-        final picturesDir = Directory('${directory!.path}/Pictures/양봉장');
-        if (!picturesDir.existsSync()) {
-          picturesDir.createSync(recursive: true);
-        }
-        String fileName =
-            'screenshot_${DateTime.now().millisecondsSinceEpoch}.png';
-        String filePath = '${picturesDir.path}/$fileName';
-        File file = File(filePath);
-        await file.writeAsBytes(image);
+  Future<void> _startRecording() async {
+    final videoStatus = await Permission.videos.status;
+
+    if (!videoStatus.isGranted) {
+      final newStatus = await Permission.videos.request();
+      if (!newStatus.isGranted) {
         ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('스크린샷 저장됨: $filePath')));
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('스크린샷 캡쳐 실패')));
+          SnackBar(content: Text('비디오 권한이 필요합니다.')),
+        );
+        return;
       }
-    } else {
-      print('22222222222222222');
+    }
+
+    try {
+      bool start = await FlutterScreenRecording.startRecordScreen("Recording", titleNotification: "녹화 중", messageNotification: "화면을 녹화하고 있습니다.");
+
+      if (!start) {
+        setState(() {
+          isRecording = false;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('녹화 시작 실패')),
+        );
+      } else {
+        setState(() {
+          isRecording = true;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        isRecording = false;
+      });
       ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('저장소 권한이 필요합니다.')));
+        SnackBar(content: Text('녹화 도중 오류 발생: $e')),
+      );
     }
   }
 
+  Future<void> _stopRecording() async {
+    try {
+      String? path = await FlutterScreenRecording.stopRecordScreen;
+
+      if (path != null) {
+        setState(() {
+          isRecording = false;
+          _outputFilePath = path;
+        });
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('녹화가 저장되었습니다: $path')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('녹화 중지 실패')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('녹화 도중 오류 발생: $e')),
+      );
+    }
+  }
+
+  Future<void> _captureScreenshot() async {
+    final photoStatus = await Permission.photos.status;
+
+    if (!photoStatus.isGranted) {
+      final newStatus = await Permission.photos.request();
+      if (!newStatus.isGranted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('사진 권한이 필요합니다.')),
+        );
+        return;
+      }
+    }
+
+    final image = await _screenshotController.capture();
+    if (image != null) {
+      final directory = await getExternalStorageDirectory();
+      final picturesDir = Directory('${directory!.path}/Pictures/양봉장');
+      if (!picturesDir.existsSync()) {
+        picturesDir.createSync(recursive: true);
+      }
+      String fileName = 'screenshot_${DateTime.now().millisecondsSinceEpoch}.png';
+      String filePath = '${picturesDir.path}/$fileName';
+      File file = File(filePath);
+      await file.writeAsBytes(image);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('스크린샷 저장됨: $filePath')),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('스크린샷 캡쳐 실패')),
+      );
+    }
+  }
 }
 
 class GridButton extends StatelessWidget {
@@ -144,11 +226,10 @@ class GridButton extends StatelessWidget {
       onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
-          border: Border.all(
-            color: blackMyStyle2,
-            width: 1.0,
-          )
-        ),
+            border: Border.all(
+              color: Colors.black,
+              width: 1.0,
+            )),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
